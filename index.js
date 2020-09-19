bb3eb766694a12f63f1dc61903e05de04d389d3a
valen9000 <67491385+valen9000@users.noreply.github.com>
2020-09-18 22:31:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 
+
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/hellodb';
 
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+
 const router = require('./routes/index');
 app.use('/', router);
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 mongoose
@@ -17,6 +21,8 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
   .catch(err => console.error(`Connection error ${err}`));
+
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
